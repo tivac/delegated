085dafe5685f0572d12c77e1f95e915cd8abadd6
Pat Cavit <pcavit@gmail.com>
2015-05-02 20:42:00 -0700

    Wrap, support object signature API

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
-function delegated(filter, fn) {
-    if(!delegated.matches) {
-        delegated.matchers.some(function(method) {
-            if(method in document.body) {
-                delegated.matches = method;
-            }
-            
-            return delegated.matches;
-        });
+(function(win, body) {
+"use strict";
+
+// Store the right method name to call
+var matches;
+
+[ "matches", "webkitMatchesSelector", "mozMatchesSelector", "msMatchesSelector" ].some(function(method) {
+    if(method in body) {
+        matches = method;
+    }
+    
+    return matches;
+});
+
+function filter(el, src, selectors) {
+    var selector;
+    
+    if(el === src) {
+        return false;
+    }
+    
+    for(selector in selectors) {
+        if(el[matches](selector)) {
+            return {
+                el : el,
+                fn : selectors[selector]
+            };
+        }
+    }
+    
+    return filter(el.parentNode, src, selectors);
+}
+
+// delegated("a.foo", fn(e, a) { ... })
+// delegated({
+//    "a.foo" : fn(e, a) { ... },
+//    ".bar"  : fn(e, el) { ... }
+// })
+function delegated(selectors, fn) {
+    var _selectors = selectors;
+    
+    if(typeof selectors === "string") {
+        _selectors = {};
+        _selectors[selectors] = fn;
     }
 
     return function(e) {
         var el = e.target,
-            match = delegated.filter(el, e.currentTarget, filter);
+            match = filter(el, e.currentTarget, _selectors);
         
         if(!match) {
            return;
         }
         
-        fn(e, match);
+        match.fn(e, match.el);
     };
 }
 
-delegated.matchers = [
-    "matches",
-    "webkitMatchesSelector",
-    "mozMatchesSelector",
-    "msMatchesSelector"
-];
-
-delegated.filter = function(el, src, filter) {
-    if(el === src) {
-        return false;
-    }
-        
-    if(el[delegated.matches](filter)) {
-        return el;
-    }
-    
-    return delegated.filter(el.parentNode, src, filter);
-};
-
 if(typeof module !== "undefined" && module.exports) {
     module.exports = delegated;
+} else {
+    win.delegated = delegated;
 }
+
+}(window, document.body));
